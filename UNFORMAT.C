768afe189059a82685545761b5067cf3ba7ef82f
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-12-08 14:30:03 +0530

    unformatted input fxn

---
+++ UNFORMAT.C
@@ -0,0 +1,51 @@
+/*
+#include<stdio.h>
+#include<conio.h>
+void main()
+	{
+		char alpha;
+		clrscr();
+		alpha = getchar();
+		printf("here alpha is \t %c", alpha);
+		getch();
+	}
+
+*/
+
+/*
+#include<stdio.h>
+#include<conio.h>
+void main()
+	{
+		char alpha;
+		clrscr();
+		alpha = getchar();
+		printf("here alpha is \t %c", alpha);
+		getch();
+	}
+*/
+
+/*
+#include<stdio.h>
+#include<conio.h>
+void main()
+	{
+		char alpha;
+		clrscr();
+		alpha = getche();
+		printf("here alpha is \t %c", alpha);
+		getch();
+	}
+*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+	{
+		char alpha[10];
+		clrscr();
+		gets(alpha);
+		printf("here alpha is \t %s", alpha);
+		getch();
+
+	}
